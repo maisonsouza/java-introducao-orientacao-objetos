0e59f9fded4727010d47a7bc65591bd339b3d646
Maison Galvão <maisongalvao144@gmail.com>
2019-03-09 16:18:10 -0400

    Cap 5 - Encapsulamento e visibilidade Atributos privados

--- bytebank/src/TestaBanco.java
+++ bytebank/src/TestaBanco.java
@@ -2,17 +2,17 @@ public class TestaBanco {
 
     public static void main(String[] args) {
         Cliente paulo = new Cliente();
-        paulo.nome = "Paulo";
-        paulo.cpf = "98225723287-87";
-        paulo.profissao = "Segurança";
+        paulo.setNome("Paulo");
+        paulo.setCpf("98225723287-87");
+        paulo.setProfissao("Segurança");
         Conta contaDoPaulo = new Conta();
         contaDoPaulo.setSaldo(100);
         contaDoPaulo.setTitular(paulo);
-        System.out.println(contaDoPaulo.getTitular().nome);
+        System.out.println(contaDoPaulo.getTitular().getNome());
         Conta contaDaMonica = new Conta();
         contaDaMonica.setTitular(new Cliente());
-        contaDaMonica.getTitular().nome = "Monica";
-        System.out.println(contaDaMonica.getTitular().nome);
+        contaDaMonica.getTitular().setNome("Monica");
+        System.out.println(contaDaMonica.getTitular().getNome());
 
     }
 }
