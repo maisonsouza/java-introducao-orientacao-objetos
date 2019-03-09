91fbda8dc00425d50d91e9c54123e84fb2794cef
Maison Galvão <maisongalvao144@gmail.com>
2019-03-09 12:38:38 -0400

    Cap 3 - Definindo Comportamento Métodos com retorno

--- bytebank/src/Conta.java
+++ bytebank/src/Conta.java
@@ -7,4 +7,14 @@ class Conta{
     void deposita(double valor){
         this.saldo+=valor;
     }
+
+    boolean saca(double valor){
+        if(this.saldo>=valor){
+            this.saldo-=valor;
+            return true;
+        }else{
+            return false;
+        }
+    }
+
 }
--- bytebank/src/CriaConta.java
+++ bytebank/src/CriaConta.java
@@ -8,6 +8,13 @@ public class CriaConta {
         System.out.println(segundaConta.saldo);
         primeiraConta.deposita(150);
         System.out.println(primeiraConta.saldo);
+        boolean conseguiu = primeiraConta.saca(275);
+        System.out.println(primeiraConta.saldo);
+        System.out.println(conseguiu);
+        conseguiu = primeiraConta.saca(76);
+        System.out.println(primeiraConta.saldo);
+        System.out.println(conseguiu);
+
 
     }
 }
